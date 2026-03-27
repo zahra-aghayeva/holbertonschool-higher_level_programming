4a5c32376c2a3307af7a2dc7f4cfcc4c950b8691
zahra-aghayeva <zehra.aqayeva777@gmail.com>
2026-03-27 20:20:32 +0000

    Fix logic and formatting for 100-my_calculator.py

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
-import sys
-from calculator_1 import add, sub, mul, div
-
 if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    a = int(sys.argv[1])
-    operator = sys.argv[2]
-    b = int(sys.argv[3])
-
-    if operator == '+':
-        print("{} {} {} = {}".format(a, operator, b, add(a, b)))
-    elif operator == '-':
-        print("{} {} {} = {}".format(a, operator, b, sub(a, b)))
-    elif operator == '*':
-        print("{} {} {} = {}".format(a, operator, b, mul(a, b)))
-    elif operator == '/':
-        print("{} {} {} = {}".format(a, operator, b, div(a, b)))
-    else:
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in ops:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    op = sys.argv[2]
+    result = ops[op](a, b)
+    print("{} {} {} = {}".format(a, op, b, result))
