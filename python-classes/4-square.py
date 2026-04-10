d60993a0ac81f649b166749275ef9735acbff201
zahra-aghayeva <zehra.aqayeva777@gmail.com>
2026-04-10 21:53:40 +0000

    Add getter and setter for size

---
+++ python-classes/4-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Defines a class Square."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0):
+        """Initializes a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Get/set the current size of the square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns the current square area."""
+        return (self.__size * self.__size)
