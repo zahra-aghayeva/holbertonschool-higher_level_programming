d03355e716363e6f1580433e10a406388a56d595
zahra-aghayeva <zehra.aqayeva777@gmail.com>
2026-04-10 21:05:39 +0000

    My first square class

---
+++ python-classes/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Bu modul Square sinfini təyin edir.
+Modul daxilində kvadratlarla bağlı əməliyyatlar aparılır.
+"""
+
+
+class Square:
+    """
+    Kvadratı təmsil edən boş bir sinif.
+    Hələlik heç bir atribut və ya metodu yoxdur.
+    """
+    pass
