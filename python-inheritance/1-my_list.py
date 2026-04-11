58d1b6d3650204d55cd3cc0e25f0f870fc11da41
zahra-aghayeva <zehra.aqayeva777@gmail.com>
2026-04-11 00:21:41 +0000

    Fix negative numbers, empty list tests and PEP8

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
 """
 This module defines a class MyList that inherits from the built-in list class.
-It provides additional functionality to print the list in a sorted manner.
 """
 
 
 class MyList(list):
     """
     MyList is a subclass of the built-in list class.
-    It includes a method to print the elements of the list in ascending order.
     """
 
     def print_sorted(self):
         """
-        Public instance method that prints the list, but sorted in ascending order.
-        All elements in the list are assumed to be of type int.
-        The original list remains unchanged after this method is called.
+        Prints the list, but sorted in ascending order.
         """
         print(sorted(self))
