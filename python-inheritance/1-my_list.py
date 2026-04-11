78ab73d24a298a183ae6e9f3acc2ae1c1c78cacd
zahra-aghayeva <zehra.aqayeva777@gmail.com>
2026-04-11 00:19:21 +0000

    Improve documentation for MyList to pass checker

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
-"""Defines an inherited list class MyList."""
+"""
+This module defines a class MyList that inherits from the built-in list class.
+It provides additional functionality to print the list in a sorted manner.
+"""
 
 
 class MyList(list):
-    """A class that inherits from list."""
+    """
+    MyList is a subclass of the built-in list class.
+    It includes a method to print the elements of the list in ascending order.
+    """
 
     def print_sorted(self):
-        """Prints the list in ascending sorted order."""
+        """
+        Public instance method that prints the list, but sorted in ascending order.
+        All elements in the list are assumed to be of type int.
+        The original list remains unchanged after this method is called.
+        """
         print(sorted(self))
